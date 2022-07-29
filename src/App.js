81541b08aeb4c58db1d08edd6ccdeabf857a5b94
Murilo Rodrigues <murilo.rodrigues@ambevtech.com.br>
2022-07-29 15:06:49 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Comentario  from './components/Comentario';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My First React Project</h1>
+      <Comentario/>
+      <Comentario/>
+      <Comentario/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Comentario.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Comentario.css';
+
+//JSX - código parecido com HTML dentro do JS.
+// class do HTML por exemplo é className
+const Comentario = () => (
+    <div className='Comentario'>
+        <div>Hello</div>
+        <div>I am a component</div>
+    </div>
+);
+
+export default Comentario;
